f846a9ccd994e3056a4d2e02f7bcf2c4591c9008
hazlo-dev <solohazlo.media@gmail.com>
2026-08-08 00:05:18 +0200

    Probar PWA

--- script.js
+++ script.js
@@ -21,3 +21,15 @@ elements.forEach((element,index)=>{
     },delays[index]);
 
 });
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("./sw.js")
+            .then(() => {
+                console.log("Service Worker registrado correctamente");
+            })
+            .catch((error) => {
+                console.error("Error registrando el Service Worker:", error);
+            });
+    });
+}
