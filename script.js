2c49df6d2e9c9971db269483a237f4bf621ffc9b
hazlo-dev <solohazlo.media@gmail.com>
2026-08-07 19:46:56 +0200

    Comprobacion movil

---
+++ script.js
@@ -0,0 +1,23 @@
+const elements = document.querySelectorAll(".hidden");
+
+const delays = [
+    200,   // Primera frase
+    300,   // Segunda frase
+    600,   // Nombre
+    900,  // Asesor de viajes
+    1100,  // WhatsApp
+    1400,  // Reservar llamada
+    1700,  // Página de reservas
+    2000,  // Beneficios
+    2300   // Reseñas
+];
+
+elements.forEach((element,index)=>{
+
+    setTimeout(()=>{
+
+        element.classList.add("show");
+
+    },delays[index]);
+
+});
