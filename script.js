de481a3270d06eb2a1bb8bf1932d9c9416fedcd1
hazlo-dev <solohazlo.media@gmail.com>
2026-08-11 10:56:32 +0200

    Arreglos bug movil

--- script.js
+++ script.js
@@ -4,21 +4,27 @@ const delays = [
     200,   // Primera frase
     300,   // Segunda frase
     600,   // Nombre
-    900,  // Asesor de viajes
+    900,   // Asesor de viajes
     1100,  // WhatsApp
     1400,  // Reservar llamada
     1700,  // Página de reservas
     2000,  // Beneficios
-    2300   // Reseñas
+    2300,  // Reseñas
+
+    2600,  // Así organizaremos tu viaje
+    2900,  // Cuéntame tu idea
+    3200,  // Busco la mejor opción
+    3500,  // Solo queda disfrutar
+    3800,  // Footer
 ];
 
-elements.forEach((element,index)=>{
+elements.forEach((element, index) => {
 
-    setTimeout(()=>{
+    setTimeout(() => {
 
         element.classList.add("show");
 
-    },delays[index]);
+    }, delays[index]);
 
 });
 
