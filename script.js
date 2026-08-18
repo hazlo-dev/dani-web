c5f318f77bfe77be10f672a114e1590955593ce0
hazlo-dev <solohazlo.media@gmail.com>
2026-08-18 23:46:07 +0200

    Empezar formulario, ajustar nombres y detalles

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const delays = [
     200,    // Primera frase
     300,    // Segunda frase
     600,    // Dani Ruiz
-    900,    // Agente de viajes independiente
+    900,    // Asesor de viajes
     1100,   // Contactar por WhatsApp
     1400,   // Reservar llamada
     1700    // Planifica tu viaje
